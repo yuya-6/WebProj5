0cc6af8fb8b39831ad254130de74e8407ba01340
yuya-6 <yuchongakufu@gmail.com>
2024-07-19 17:01:07 +0900

    HelloStruts機能2回目

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,12 +1,15 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
 
-</body>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>INDEX</title>
+	</head>
+	<body>
+		<s:form action="HelloStrutsAction">
+			<s:submit value="HelloStruts"/>
+		</s:form>
+	</body>
 </html>
--- src/com/diworksdev/webproj5/util/DBConnector.java
+++ src/com/diworksdev/webproj5/util/DBConnector.java
@@ -1,5 +1,40 @@
 package com.diworksdev.webproj5.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class DBConnector {
+	/**
+	* JDBC ドライバー名
+	*/
+	private static String driverName="com.mysql.jdbc.Driver";
 
+	/**
+	* データベース接続 URL
+	*/
+	private static String url="jdbc:mysql://localhost/testdb";
+
+	/**
+	* データベース接続ユーザ名
+	*/
+	private static String user="root";
+
+	/**
+	* データベース接続パスワード
+	*/
+	private static String password="0114Yuya";
+
+	public Connection getConnection() {
+		Connection con=null;
+		try{
+			Class.forName(driverName);
+			con=DriverManager.getConnection(url,user,password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace() ;
+		} catch (SQLException e) {
+			e.printStackTrace() ;
+		}
+		return con ;
+	}
 }
